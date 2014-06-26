c4196df92f6ebbf1138b502461a6afa51dc782d8
Doron <devdoron@gmail.com>
2014-06-25 20:49:04 -0700

    Fragment works. Added some fields to Crime.

--- src/com/learning/criminalintent/Crime.java
+++ src/com/learning/criminalintent/Crime.java
@@ -1,14 +1,31 @@
 package com.learning.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 	private UUID mId;
 	private String mTitle;
+	private Date mDate; // The date a crime occurred.
+	private boolean mSolved; // Was the crime solved?
+
 
 	public Crime() {
 		// Generate unique id
 		mId = UUID.randomUUID();
+		mDate = new Date();
+	}
+
+	public String getmTitle() {
+		return mTitle;
+	}
+
+	public void setmTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+
+	public UUID getmId() {
+		return mId;
 	}
 
 }
