82e94601f370784c3876f0e4888a18e1715219ad
Doron <devdoron@gmail.com>
2014-06-25 21:03:06 -0700

    Added Details, date,  checkbox

--- src/com/learning/criminalintent/Crime.java
+++ src/com/learning/criminalintent/Crime.java
@@ -16,15 +16,31 @@ public class Crime {
 		mDate = new Date();
 	}
 
-	public String getmTitle() {
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date mDate) {
+		this.mDate = mDate;
+	}
+
+	public boolean isSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(boolean mSolved) {
+		this.mSolved = mSolved;
+	}
+
+	public String getTitle() {
 		return mTitle;
 	}
 
-	public void setmTitle(String mTitle) {
+	public void setTitle(String mTitle) {
 		this.mTitle = mTitle;
 	}
 
-	public UUID getmId() {
+	public UUID getId() {
 		return mId;
 	}
 
--- src/com/learning/criminalintent/CrimeFragment.java
+++ src/com/learning/criminalintent/CrimeFragment.java
@@ -7,11 +7,17 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 	private Crime mCrime;
 	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,10 +31,11 @@ public class CrimeFragment extends Fragment {
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 		mTitleField = (EditText) v.findViewById(R.id.crime_title);
 		mTitleField.addTextChangedListener(new TextWatcher() {
-			
+
 			@Override
-			public void onTextChanged(CharSequence c, int start, int before, int count) {
-				mCrime.setmTitle(c.toString());
+			public void onTextChanged(CharSequence c, int start, int before,
+					int count) {
+				mCrime.setTitle(c.toString());
 
 			}
 
@@ -45,6 +52,21 @@ public class CrimeFragment extends Fragment {
 			}
 
 		});
+
+		mDateButton = (Button) v.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+
+		mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox
+				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
+						// Set the crime's solved property
+						mCrime.setSolved(isChecked);
+					}
+				});
+
 		return v;
 	}
 }
