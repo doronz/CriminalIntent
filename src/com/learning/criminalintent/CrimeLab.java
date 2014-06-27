e2b8087ec8a757a783b962edb9690b2d53833d59
Doron <devdoron@gmail.com>
2014-06-26 23:25:48 -0700

    Created CrimeLab, and filled with 100 arbitrary crimes.

---
+++ src/com/learning/criminalintent/CrimeLab.java
@@ -0,0 +1,25 @@
+package com.learning.criminalintent;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+
+public class CrimeLab {
+	private ArrayList<Crime> mCrimes;
+	
+	private static CrimeLab sCrimeLab;
+	private Context mAppContext;
+	
+	private CrimeLab(Context appContext) {
+		mAppContext = appContext;
+		mCrimes = new ArrayList<Crime>();
+	}
+	
+	public static CrimeLab get(Context c) {
+		if (sCrimeLab == null) {
+			sCrimeLab = new CrimeLab(c.getApplicationContext());
+		}
+		return sCrimeLab;
+	}
+	
+}
